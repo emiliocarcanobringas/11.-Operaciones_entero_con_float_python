faf3bf2fae3b7a990e0d20483ac9ac219dcb7857
Emilio Carcaño Bringas <emiliocarcano@uninaciones.edu.mx>
2020-12-03 20:04:41 -0600

    Add files via upload

---
+++ 11.-Operaciones_entero_con_float_python.py
@@ -0,0 +1,12 @@
+# Este programa muestra la suma de dos variables, de tipo int y float
+
+print("Este programa muestra la suma de dos variables, de tipo int y float")
+print("También muestra que la variable que realiza la operación es de tipo float")
+numero1 = 7
+numero2 = 3.1416
+sumadeambos = numero1 + numero2
+print("El resultado de la suma es: ")
+print(sumadeambos)
+print(type(sumadeambos))
+
+# Este programa fue escrito por Emilio Carcaño Bringas
